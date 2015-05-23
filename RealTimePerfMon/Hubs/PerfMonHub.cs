b7c2d5b66bbfdbc34572c2246b6765b8be3a880d
Ashish Yengkhom <yashishraj@gmail.com>
2015-05-23 12:23:04 -0400

    Initial Commit
    Demo Send messages method.

---
+++ RealTimePerfMon/Hubs/PerfMonHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RealTimePerfMon.Hubs
+{
+    public class PerfMonHub : Hub
+    {
+        public void Send(string message)
+        {
+            //broadcast to all clients. Note that All is a dynamic method
+            //SignalR makes this look like an RPC call, but the SignalR library is going to create the newMessage
+            //Javascript method client side when it packages this up.
+            Clients.All.newMessage(
+                "Hi from " + Context.User.Identity .Name + ": " + message
+                );
+        }
+    }
+}
---
+++ RealTimePerfMon/Scripts/perfMon.js
@@ -0,0 +1,43 @@
+﻿(function myfunction() {
+
+    //SignalR is a jquery plugin so augments $ and gives you a proxy on the client for the Server hub
+    var perfMonHub = $.connection.perfMonHub;
+    $.connection.hub.logging = true;
+
+    //This call is async and returns a promise that we can hook into from the client
+    $.connection.hub.start();
+
+    //now add the newMessage method on the client that was dynamically created in the server
+    perfMonHub.client.newMessage = function (message) {
+        vm.addMessage(message);
+    };
+
+    var ViewModel = function () {
+        var self = this;
+        self.message = ko.observable(""),
+        self.messages = ko.observableArray()
+    };
+
+    //create method on my view model to call the server Send method in the Hub.
+    ViewModel.prototype = {
+        sendMessage: function () {
+            var self = this;
+            perfMonHub.server.send(self.message());
+
+            //clear out once message is sent
+            self.message("");
+        },
+        addMessage: function (message) {
+            var self = this;
+            self.messages.push(message);
+        }
+    }
+
+    //create an instance of the vm
+    var vm = new ViewModel();
+
+    //Finally, wire this up when DOM is ready
+    $(function () {
+        ko.applyBindings(vm);
+    });
+}());
